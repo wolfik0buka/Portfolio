6fdecd94ae3451a517d36090c7bbe63be48ddd2f
Egor Seleznev <molodoi_padavan@mail.ru>
2018-11-06 23:14:27 +0300

    подготовка к работе

--- Maket2/gulpfile.js
+++ Maket2/gulpfile.js
@@ -7,7 +7,7 @@ var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	babel = require('gulp-babel'),
-	sourceMaps = require('gulp-sourcemaps');
+	sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('browser-sync', function(){
 	browserSync({
@@ -19,27 +19,33 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('styles', function(){
-	return gulp.src('app/scss/**/*.(sc|sa)ss')
+	return gulp.src('app/scss/**/*scss')
 	.pipe(sass({ outputStyle: 'expanded' }))
 	.pipe(rename({suffix: '.min', prefix : '' }))
 	.pipe(autoprefixer(['cover 99%']))
-	.pipe(cleanCss({level:{1:{ specialComments: 0 }}}))
-	.pipe(gulp.desr('app/css'))
+	// .pipe(cleanCss({level:{1:{ specialComments: 0 }}}))
+	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.stream());
 });
 
 gulp.task("javaScript", function () {
-  return gulp.src("src/**/*.js")
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(concat("all.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("app/js"))
-    .pipe(browserSync.reload({ stream: true }));
+  return gulp.src([
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'node_modules/wowjs/dist/wow.min.js',
+      'app/js/*.js'])
+      .pipe(sourcemaps.init())
+      .pipe(babel())
+      .pipe(concat("all.js"))
+      .pipe(sourcemaps.write("."))
+      .pipe(gulp.dest("app/dest/"))
+      .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('watch', ['javaScript','styles', 'browser-sync' ], function(){
-	gulp.watch('app/scss/**/*.(sc|sa)ss', ['styles']);
-	gulp.watch(['app/js/common.js'], ['javaScript']);
+	gulp.watch('app/scss/**/*scss', ['styles']);
+	gulp.watch(['app/js/site.js'], ['javaScript']);
 	gulp.watch('app/*.html', browserSync.reload)
-})
+});
+
+gulp.task('default', ['watch']);
